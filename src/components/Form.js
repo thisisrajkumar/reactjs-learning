133f9923b285a5dda9c46a20539e789de1b7922e
Thangavel <rajkumar.thangavel@atos.net>
2020-06-11 17:23:11 +0530

    FolderStructure changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import '../../css/style.css'
+import '../css/style.css'
 
 class Form extends Component {
   constructor() {
